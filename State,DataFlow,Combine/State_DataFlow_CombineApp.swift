8ea4f60ca7b8774a2347b17e41aabeb67e77247d
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-10-29 22:42:31 +0300

    Initial Commit

---
+++ State,DataFlow,Combine/State_DataFlow_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_DataFlow_CombineApp.swift
+//  State,DataFlow,Combine
+//
+//  Created by Александр on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct State_DataFlow_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
